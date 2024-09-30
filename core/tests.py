332c211842868e4419880dca2a89387257f32799
Alexsander Santos <61764134+Alexsander-oml@users.noreply.github.com>
2024-09-30 18:04:56 +0000

    adicionando testes

--- core/tests.py
+++ core/tests.py
@@ -1,9 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
-
 class HomeTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/')
     def test_get(self):
-        response = self.client.get('/')
-        self.assertEqual(200, response.status_code)
-        
+        '''
+        Testa se a página inicial retorna status code 200
+        '''
+        self.assertEqual(200, self.response.status_code)
+    def test_template(self):
+        self.assertTemplateUsed(self.response, 'index.html')
