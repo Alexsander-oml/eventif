c85b67eedab15bc7b9be8f37246b12e61d1f5597
Alexsander Santos <61764134+Alexsander-oml@users.noreply.github.com>
2024-11-04 18:24:10 +0000

    realizar inscrição

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,23 +1,30 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from subscription.forms import SubscriptionForm
+from subscriptions.forms import SubscriptionForm
 from django.core import mail
-# Create your views here.
+from django.template.loader import render_to_string
+from django.contrib import messages
+
 
 def subscribe(request):
-    if request.method == 'POST'
-       form = SubscriptionForm(request.POST)
-       
-    if form.is_valid()
-        
-       body = render_to_string('subscriptions;subscription_email.txt', form.cleaned_data)
-    
-       email = mail.send_mail('Confirmação de inscrição!', Message, 'contato@eventif.com.br', ['contato@eventif.com.br', form.cleaned_data['email']])
-       return HttpResponseRedirect('/inscricao/')
-    else:  
     context = {'form': SubscriptionForm()}
     return render(request, 'subscriptions/subscription_form.html', context)
-
+    if request.method == 'POST':
+        form = SubscriptionForm(request.POST)
+        if form.is_valid():
+            body = render_to_string(
+                'subscriptions/subscription_email.txt', form.cleaned_data)
+            email = mail.send_mail('Confirmação de inscrição!',
+                                   body,
+                                   'contato@eventif.com.br',
+                                   ['contato@eventif.com.br', form.cleaned_data['email']])
+            messages.success(request, 'Inscrição realizada com sucesso!')
+            return HttpResponseRedirect('/inscricao/')
+        else:
+            return render(request, 'subscriptions/subscription_form.html', {'form': form})
+    else:
+        context = {'form': SubscriptionForm()}
+        return render(request, 'subscriptions/subscription_form.html', context)
 
 Message = '''
 Olá! Tudo bem?
