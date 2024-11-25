eacca88959c837740991858dab237100c6e9d5f3
Alexsander Santos <61764134+Alexsander-oml@users.noreply.github.com>
2024-11-25 02:18:11 +0000

    removing, organizing and contact

--- contact/tests/test_form_contact.py
+++ contact/tests/test_form_contact.py
@@ -3,34 +3,27 @@ from django.test import TestCase
 from contact.forms import ContactForm
 
 
-class TestFormContact(TestCase):
-    def setUp(self):
-        self.response = self.client.get('/contato/')
-
-    def test_contact_form_has_fields(self):
+class ContactFormTest(TestCase):
+    def test_form_has_fields(self):
         form = ContactForm()
         self.assertTupleEqual(tuple(form.fields), ('name', 'email', 'phone', 'message'))
 
-    def test_view_form_inherits_contact_form(self):
-        form = self.response.context.get('form')
-        self.assertIsInstance(form, ContactForm)
+    def test_name_must_be_capitalized(self):
+        form = self.make_validated_form(name='PEDRO machado')
+        self.assertEqual(form.cleaned_data['name'], 'Pedro Machado')
 
-    def test_view_form_has_tags(self):
-        tags = (('<form', 1), ('<input', 5), ('<textarea', 1))
-        for tag, count in tags:
-            with self.subTest():
-                self.assertContains(self.response, tag, count)
+    def test_phone_is_optional(self):
+        form = self.make_validated_form(phone='')
+        self.assertFalse(form.errors)
 
-    def test_view_form_inputs_have_attributes(self):
-        attribs = (
-            ('type="text"', 2),
-            ('type="email"', 1),
-            ('type="submit"', 1),
-            ('type="hidden"', 1),
+    def make_validated_form(self, **kwargs):
+        valid = dict(
+            name='Pedro Machado',
+            email='pedro.machado@mail.com',
+            phone='053-98429-5133',
+            message='Lorem ipsum dolor sit amet',
         )
-        for attrib, count in attribs:
-            with self.subTest():
-                self.assertContains(self.response, attrib, count)
-
-    def test_view_form_has_csrf(self):
-        self.assertContains(self.response, 'csrfmiddlewaretoken')
+        data = dict(valid, **kwargs)
+        form = ContactForm(data)
+        form.is_valid()
+        return form
