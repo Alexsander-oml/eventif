3eb8a37a75b6b2a0b4f220931533bf2e3e5d0b37
Alexsander Santos <61764134+Alexsander-oml@users.noreply.github.com>
2024-11-25 02:35:24 +0000

    phone value

--- contact/tests/test_model_contact.py
+++ contact/tests/test_model_contact.py
@@ -10,6 +10,7 @@ class ContactModelTest(TestCase):
         self.data = Contact(
             name='Alex Santos',
             email='alex.santos@mail.com',
+            phone='053-91234-5678',
             message='Lorem ipsum dolor sit amet',
         )
         self.data.save()
@@ -20,11 +21,8 @@ class ContactModelTest(TestCase):
     def test_created_at(self):
         self.assertIsInstance(self.data.created_at, datetime)
 
-    def test_phone_default_nao_informado(self):
-        self.assertEqual(self.data.phone, 'Não informado')
-
     def test_replied_default_false(self):
         self.assertFalse(self.data.replied)
 
     def test_str(self):
-        self.assertEqual(str(self.data), 'Pedro Machado')
+        self.assertEqual(str(self.data), 'Alex Santos')
