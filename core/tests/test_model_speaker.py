5059ede4f2f145864ecb97ab44514053529b9a70
Alexsander Santos <61764134+Alexsander-oml@users.noreply.github.com>
2024-12-10 17:44:06 +0000

    finalizando projeto

--- core/tests/test_model_speaker.py
+++ core/tests/test_model_speaker.py
@@ -1,6 +1,5 @@
-from django.shortcuts import resolve_url as r
 from django.test import TestCase
-
+from django.shortcuts import resolve_url as r
 from core.models import Speaker
 
 
@@ -9,20 +8,20 @@ class SpeakerModelTest(TestCase):
         self.speaker = Speaker.objects.create(
             name='Grace Hopper',
             slug='grace-hopper',
-            website='https://pt.wikipedia.org/wiki/Grace_Hopper',
-            photo='https://cleberfonseca.com.br/img/hopper.jpeg',
-            description='Programadora e almirante.',
+            photo='https://abre.ai/hopper-pic',
+            website='https://abre.ai/hopper-site',
+            description='Programadora e almirante.'
         )
 
     def test_create(self):
         self.assertTrue(Speaker.objects.exists())
 
-    def test_website_can_be_blank(self):
-        field = Speaker._meta.get_field('website')
-        self.assertTrue(field.blank)
-
     def test_description_can_be_blank(self):
         field = Speaker._meta.get_field('description')
+        self.assertTrue(field.blank)
+
+    def test_website_can_be_blank(self):
+        field = Speaker._meta.get_field('website')
         self.assertTrue(field.blank)
 
     def test_str(self):
