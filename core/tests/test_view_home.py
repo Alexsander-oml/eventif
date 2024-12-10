fd0b43b9011d91aed17863c440d300098f399e04
Alexsander Santos <61764134+Alexsander-oml@users.noreply.github.com>
2024-12-10 17:40:06 +0000

    keynote speakers

--- core/tests/test_view_home.py
+++ core/tests/test_view_home.py
@@ -2,30 +2,29 @@ from django.test import TestCase
 from django.shortcuts import resolve_url as r
 
 
-class TestHome(TestCase):
-    fixtures = ['keynotes.json']
-
+class HomeTest(TestCase):
     def setUp(self):
         self.response = self.client.get(r('home'))
 
-    def test_home(self):
-        self.assertEqual(self.response.status_code, 200)
+    def test_get(self):
+        '''
+        Testa se a página inicial retorna status code 200
+        '''
+        self.assertEqual(200, self.response.status_code)
 
-    def test_template_used(self):
+    def test_template(self):
         self.assertTemplateUsed(self.response, 'index.html')
 
-    def test_subscription_link(self):
-        expect = 'href="{}"'.format(r('subscriptions:new'))
-        self.assertContains(self.response, expect)
+    def test_link_subscription(self):
+        self.assertContains(
+            self.response, 'href="{}"'.format(r('subscriptions:new')))
 
     def test_speakers(self):
         contents = [
             'Grace Hopper',
-            'https://cleberfonseca.com.br/img/hopper.jpeg',
-            'href="{}"'.format(r('speaker_detail', slug='grace-hopper')),
+            'https://abre.ai/hopper-pic',
             'Alan Turing',
-            'https://cleberfonseca.com.br/img/turing.jpeg',
-            'href="{}"'.format(r('speaker_detail', slug='alan-turing')),
+            'https://abre.ai/turing-pic'
         ]
         for expected in contents:
             with self.subTest():
@@ -33,8 +32,4 @@ class TestHome(TestCase):
 
     def test_speakers_link(self):
         expected = 'href="{}#speakers"'.format(r('home'))
-        self.assertContains(self.response, expected)
-
-    def test_talks_link(self):
-        expected = 'href="{}"'.format(r('talk_list'))
         self.assertContains(self.response, expected)
--- core/views.py
+++ core/views.py
@@ -1,21 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-
-from core.models import Speaker, Talk
+from django.shortcuts import render
 
 
 def home(request):
-    speakers = Speaker.objects.all()
+    speakers = [
+        {'name': 'Grace Hopper', 'photo': 'https://abre.ai/hopper-pic'},
+        {'name': 'Alan Turing', 'photo': 'https://abre.ai/turing-pic'}
+    ]
     return render(request, 'index.html', {'speakers': speakers})
-
-
-def speaker_detail(request, slug):
-    speaker = get_object_or_404(Speaker, slug=slug)
-    return render(request, 'core/speaker_detail.html', {'speaker': speaker})
-
-
-def talk_list(request):
-    context = {
-        'morning_talks': Talk.objects.filter(start__lt='12:00'),
-        'afternoon_talks': Talk.objects.filter(start__gte='12:00'),
-    }
-    return render(request, 'core/talk_list.html', context)
